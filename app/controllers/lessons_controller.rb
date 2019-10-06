bb21463ee369d9bafe9868188509d4e8e0f75cf0
Sirdmu <din.siraj7@gmail.com>
2019-10-06 02:57:13 +0000

    Set up permissions and authentications

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,18 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, only: [:show]
+
   def show
   end
 
   private
+
+  def require_authorized_for_current_lesson
+    unless current_user.enrolled_in?(current_lesson.section.course)
+      # render plain: “Unauthorized”, status: :unauthorized
+      redirect_to courses_path, alert: 'You are not currently enrolled in this Course'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
